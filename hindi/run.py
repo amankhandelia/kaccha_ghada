d1621eb42ae519937c8841f8c5e0591a0c690c2b
amank <opensourceaman@gmail.com>
2021-07-23 15:17:02 +0000

    FR: added functions to address the character level tokenization in Hindi

--- hindi/run.py
+++ hindi/run.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils import test_tokenization
+from utils import test_tokenization, save_readable_vocab
 
 def run_test_tokenization(config_file = '/home/khandelia1000/kaccha_ghada/hindi/mlm_test_config.csv', output_file='/home/khandelia1000/tokens/hindi_tokens.csv'):
     
@@ -18,9 +18,19 @@ def run_test_tokenization(config_file = '/home/khandelia1000/kaccha_ghada/hindi/
     # save outputs to csv
     pd.DataFrame(outputs).to_csv(output_file)
 
+def save_all_vocabs():
+    # read models csv and remove irrelavent columns
+    models_df = pd.read_csv(config_file)
+    del models_df['display_name'], models_df['revision']
+    
+    for i in range(len(models_df)):
+        kwargs = models_df.iloc[i].to_dict()
+        tokenizer = AutoTokenizer.from_pretrained(**kwargs)
+        readable_csv_path = '/home/khandelia1000/tokens/{}_vocab.csv'.format(kwargs['model_name'].split('/')[1])
+        save_readable_vocab(tokenizer, readable_csv_path)
+
 if __name__ == '__main__':
     run_test_tokenization()
-    # readable_csv_path = '/home/khandelia1000/tokens/{}_vocab.csv'.format(model_name.split('/')[1])
     
     
     
--- hindi/utils.py
+++ hindi/utils.py
@@ -3,8 +3,7 @@ import pandas as pd
 from typing import List
 
 
-def save_readable_vocab(model_name, readable_csv_path, use_fast=False):
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=use_fast)
+def save_readable_vocab(tokenizer, readable_csv_path):
     if isinstance(tokenizer, RobertaTokenizer):
         tokens_df = pd.DataFrame({'tokens':list(tokenizer.encoder.keys())})
     else:
@@ -18,6 +17,14 @@ def get_readable_tokenization(text:str, tokenizer:AutoTokenizer):
     readable_tokens = [tokenizer.convert_tokens_to_string([token]) for token in tokens]
     return readable_tokens
 
+def get_readable_merges(merges:dict, tokenizer:AutoTokenizer):
+    readable_merges = {}
+    for tokens, idx in merges.items():
+        readable_tokens = [tokenizer.convert_tokens_to_string([token]) for token in tokens]
+        tokens = tuple(readable_tokens)
+        readable_merges[tokens] = idx
+    return readable_merges
+
 def get_identity_tokenization(text:str, tokenizer:AutoTokenizer):
     tokens = tokenizer.tokenize(text)
     return tokenizer.convert_tokens_to_string(tokens)
@@ -29,7 +36,43 @@ def test_tokenization(model_name:str, from_flax:bool, use_fast:bool, add_prefix_
         output.append((text, get_readable_tokenization(text, tokenizer), get_identity_tokenization(text, tokenizer)))
     return output
 
+def devnagri_unicode_block():
+    devnagri_block_len = 128
+    devnagri_block_start = 2304
+    devnagri_chars = [chr(i) for i in range(devnagri_block_start, devnagri_block_start + devnagri_block_len)]
+    return devnagri_chars
 
-   
-
+def get_all_devnagri_char_by_category(category=None):
+    matra = ['ँ', 'ं', 'ः', '़', 'ा', 'ि', 'ी', 'ु', 'ू', 'ृ', 'ॄ', 'ॅ', 'े', 'ै', 'ॉ', 'ो', 'ौ', '्']
+    rare_matra = ['ऀ', 'ॖ', 'ॗ', 'ॢ', 'ॣ', 'ॎ', 'ॏ', '॑', '॒', '॓', '॔', 'ॕ', 'ऺ', 'ऻ', 'ॊ', 'ॆ']
+    swara = ['अ', 'आ', 'इ', 'ई', 'उ', 'ऊ', 'ऋ', 'ऍ', 'ऎ', 'ए', 'ऐ', 'ऑ', 'ओ', 'औ']
+    rare_vowels = ['ऄ', 'ऒ', 'ऌ']
     
+    vyanjan = ['क', 'ख', 'ग', 'घ', 'ङ', 'च', 'छ', 'ज', 'झ', 'ञ', 'ट', 'ठ', 'ड', 'ढ', 'ण', 'त', 'थ', 'द', 'ध', 'न', 'प', 'फ', 'ब', 'भ', 'म', 'य', 'र', 'ल', 'ळ', 'ऴ', 'व', 'श', 'ष', 'स', 'ह', 'ॠ']
+    consonants_with_nukta = ['ऩ', 'ऱ', 'क़', 'ख़', 'ग़', 'ज़', 'ड़', 'ढ़', 'फ़', 'य़']
+    sanyukta_akshar = ['क्ष','त्र','ज्ञ','श्र']
+    rare_characters = ['ॡ', 'ॾ', 'ॿ', 'ॲ', 'ॳ', 'ॴ', 'ॵ', 'ॶ', 'ॷ', 'ॸ', 'ॹ', 'ॺ', 'ॻ', 'ॼ']
+    digits = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९']
+    symbols = ['।', '॥', 'ॽ', 'ॐ', 'ऽ', '॰', 'ॱ']
+    if category == 'matras':
+        return matra
+    elif category == 'vowels':
+        return swara
+    elif category == 'consonants':
+        return vyanjan
+    elif category == 'digits':
+        return digits
+    elif category == 'rare_characters':
+        return rare_characters
+    elif category == 'symbols':
+        return symbols
+    elif category == 'consonants_with_nukta':
+        return consonants_with_nukta
+    elif category == 'sanyukta_akshar':
+        return sanyukta_akshar
+    elif category == 'rare_vowels':
+        return rare_vowels
+    elif category == 'rare_matra':
+        return rare_matra
+    else:
+        return (matra, swara, vyanjan, consonants_with_nukta, rare_characters, digits, symbols)
